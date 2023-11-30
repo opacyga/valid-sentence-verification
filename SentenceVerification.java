4647d7c8ea00b69c934991fcd8c44ff458b67243
40373667 <40373667@ads.qub.ac.uk>
2023-11-30 20:54:24 +0000

    Added skeleton methods to SentenceVerification

--- SentenceVerification.java
+++ SentenceVerification.java
@@ -1,2 +1,35 @@
 public class SentenceVerification {
+
+    private static String input;
+
+    public static boolean isSentenceValid(String input) {
+        SentenceVerification.input = input;
+
+        return false;
+    }
+
+    private static boolean capitalLetterIsAtBeginning() {
+
+        return false;
+    }
+
+    private static boolean evenNumberQuotationMarks() {
+
+        return false;
+    }
+
+    private static boolean stringTerminated() {
+
+        return false;
+    }
+
+    private static boolean periodCharOnlyAtEnd() {
+
+        return false;
+    }
+
+    private static boolean numbersBelow13SpelledOut() {
+
+        return false;
+    }
 }
