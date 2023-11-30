9a570ab8e1f5365c24a36c92e12396588428549b
40373667 <40373667@ads.qub.ac.uk>
2023-11-30 21:00:08 +0000

    Added comments to isSentenceValid method

--- SentenceVerification.java
+++ SentenceVerification.java
@@ -2,6 +2,17 @@ public class SentenceVerification {
 
     private static String input;
 
+    /**
+     * This checks if the string input:
+     *  * Has a capital letter at the beginning
+     *  * Has an even number of quotation marks
+     *  * Ends with a proper termination character (e.g. ".", "?")
+     *  * Has no period character other than the last character
+     *  * Has numbers below 13 spelled out
+     *
+     * @param input the string to be checked for if it is "valid"
+     * @return false if string is valid, true if not
+     */
     public static boolean isSentenceValid(String input) {
         SentenceVerification.input = input;
 
