c216cc50943cce3f0239e46950faec0bd68078ed
40373667 <40373667@ads.qub.ac.uk>
2023-11-30 21:52:42 +0000

    added test logic to startIsLowerCase

--- tests/SentenceVerificationIT.java
+++ tests/SentenceVerificationIT.java
@@ -1,6 +1,9 @@
 package tests;
 
 import org.junit.Test;
+import src.SentenceVerification;
+
+import static org.junit.Assert.*;
 
 public class SentenceVerificationIT {
     static String input;
@@ -8,7 +11,8 @@ public class SentenceVerificationIT {
 
     @Test
     public void startIsLowerCase() {
-
+        input = "this is an invalid sentence";
+        assertFalse(SentenceVerification.isSentenceValid(input));
     }
 
     @Test
