b419b3b6695502f70f97a529f9fde68d00268b81
40373667 <40373667@ads.qub.ac.uk>
2023-11-30 21:06:18 +0000

    created isCapitalLetter skeleton and added comments to capitalLetterIsAtBeginning

--- SentenceVerification.java
+++ SentenceVerification.java
@@ -8,8 +8,16 @@ public class SentenceVerification {
         return false;
     }
 
+    /**
+     * Checks if the character at the beginning is a capital letter
+     * @return true if it is a capital letter, false otherwise
+     */
     private static boolean capitalLetterIsAtBeginning() {
+        return isCapitalLetter(SentenceVerification.input.charAt(0));
+    }
 
+
+    private static boolean isCapitalLetter(char first) {
         return false;
     }
 
